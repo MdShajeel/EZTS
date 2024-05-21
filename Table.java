efe8bf78178c54d3f9cfc6f9ee751c10ba12c637
MdShajeel <94459156+MdShajeel@users.noreply.github.com>
2024-05-21 23:03:31 +0530

    Multiplication Tables code

---
+++ Table.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+import java.util.*;
+
+public class Table
+
+{
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+
+        int i;
+
+        int n=sc.nextInt();
+
+        for(i=1;i<=10;i++)
+
+        {
+
+            System.out.println(n+"*"+i+"="+(n*i));
+
+        }  
+
+    }
+
+}
+
