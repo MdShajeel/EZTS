1afb2a0d736e03d98cbbac32a8133c0dcd4bfc49
MdShajeel <94459156+MdShajeel@users.noreply.github.com>
2024-05-30 12:26:01 +0530

    Hospital

--- Hospital/Main.java
+++ Hospital/Main.java
@@ -2,50 +2,50 @@ package Hospital;
 
 import java.util.Scanner;
 
-public class Main 
-{public static void main(String[] args) {
-	 Scanner s=new Scanner(System.in);
-	 int age,b,r,f,nofp;
-	 String name,gender,spe,de;
-	 System.out.println("Enter Name=");
-	 name=s.next();
-	 System.out.println("Enter Age=");
-	 age=s.nextInt();
-	 System.out.println("Enter Gender=");
-	 gender=s.next();
-	 System.out.println("Enter 1 ---if you are Docter ,Enter 2 ---if you are Patient ,Enter 3 ---if you are Nurse");
-	 int c1;
-	 c1=s.nextInt();
-	 switch(c1) {
-		 case 1:
-			 System.out.println("Enter Your specialation=");
-			 spe=s.next();
-			 Doctor d=new Doctor(name,age,gender,spe);
-			 System.out.println("Doctor Details:");
-			 d.docdisplay();
-			 break;
-		 case 2:
-			 System.out.println("Enter Your Disease:");
-			 de=s.next();
-			 System.out.println("Enter Your Bed No:");
-			 b=s.nextInt();
-			 Patient p=new Patient(name,age,gender,de,b);
-			 System.out.println("Patient Details=");
-			 p.patdisplay();
-			 break;
-		 case 3:
-			 System.out.println("Enter Floor NO");
-			 f=s.nextInt();
-			 System.out.println("Enter Room NO");
-			 r=s.nextInt();
-			 System.out.println("Enter No of patients allocated to nurse=");
-			 nofp=s.nextInt();
-			 Nurse n=new Nurse(name,age,gender,f,r,nofp);
-			 System.out.println("Nurse Details:");
-			 n.nurdisplay();
-			 break;
-		default:
-			System.out.println("You have entered invalid input...");
-	 } 
-}
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int age, bedNumber, floorNumber, numberOfPatients;
+        String name, gender, specialization, disease;
+        System.out.println("Enter Name=");
+        name = scanner.next();
+        System.out.println("Enter Age=");
+        age = scanner.nextInt();
+        System.out.println("Enter Gender=");
+        gender = scanner.next();
+        System.out.println("Enter 1 ---if you are Doctor, Enter 2 ---if you are Patient, Enter 3 ---if you are Nurse");
+        int choice;
+        choice = scanner.nextInt();
+        switch (choice) {
+            case 1:
+                System.out.println("Enter Your specialization=");
+                specialization = scanner.next();
+                Doctor doctor = new Doctor(name, age, gender, specialization);
+                System.out.println("Doctor Details:");
+                doctor.display();
+                break;
+            case 2:
+                System.out.println("Enter Your Disease:");
+                disease = scanner.next();
+                System.out.println("Enter Your Bed No:");
+                bedNumber = scanner.nextInt();
+                Patient patient = new Patient(name, age, gender, disease, bedNumber);
+                System.out.println("Patient Details=");
+                patient.display();
+                break;
+            case 3:
+                System.out.println("Enter Floor Number:");
+                floorNumber = scanner.nextInt();
+                System.out.println("Enter Room Number:");
+                int roomNumber = scanner.nextInt();
+                System.out.println("Enter Number of Patients allocated to Nurse:");
+                numberOfPatients = scanner.nextInt();
+                Nurse nurse = new Nurse(name, age, gender, floorNumber, roomNumber, numberOfPatients);
+                System.out.println("Nurse Details:");
+                nurse.display();
+                break;
+            default:
+                System.out.println("You have entered invalid input...");
+        }
+    }
 }
