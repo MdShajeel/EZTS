c633275190c7abface7ec16ed86c8b5bfeba483b
MdShajeel <94459156+MdShajeel@users.noreply.github.com>
2024-05-30 12:29:32 +0530

    Hospital

--- Hospital/Nurse.java
+++ Hospital/Nurse.java
@@ -1,19 +1,19 @@
 package Hospital;
-public class Nurse extends Person
-{
-    private int roomno,floorno,noofp;
-    public Nurse(String str,int a,String g,int r,int f,int p) 
-    {
-        super(str,a,g);
-        this.roomno=r;
-        this.floorno=f;
-        this.noofp=p;
+
+public class Nurse extends Person {
+    private int roomNumber, floorNumber, numberOfPatients;
+
+    public Nurse(String name, int age, String gender, int roomNumber, int floorNumber, int numberOfPatients) {
+        super(name, age, gender);
+        this.roomNumber = roomNumber;
+        this.floorNumber = floorNumber;
+        this.numberOfPatients = numberOfPatients;
     }
-    void nurdisplay() 
-    {
-    	super.perdisplay();
-        System.out.println("Floor No=" + this.floorno);
-        System.out.println("Room No=" + this.roomno);
-        System.out.println("No of patients allocated to Nurse=" + this.noofp);
+
+    void display() {
+        super.display();
+        System.out.println("Floor Number: " + this.floorNumber);
+        System.out.println("Room Number: " + this.roomNumber);
+        System.out.println("Number of Patients allocated to Nurse: " + this.numberOfPatients);
     }
 }
