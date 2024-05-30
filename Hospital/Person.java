956237ac4232564e59da866ccc76f371e1994d48
MdShajeel <94459156+MdShajeel@users.noreply.github.com>
2024-05-30 12:31:02 +0530

    Hospital

--- Hospital/Person.java
+++ Hospital/Person.java
@@ -1,19 +1,18 @@
 package Hospital;
-public class Person {
-	private int age;
-	   private String name,gender;
-	   public Person(String str,int a,String g)
-	   {
-		   this.name=str;
-		   this.age=a;
-		   this.gender=g;
-	   }
-	   void perdisplay() {
-	       System.out.println("Name: " +this.name);
-	       System.out.println("Age: " + this.age);
-	       System.out.println("Gender: " + this.gender);
-	   }
-	   
-	 
 
+public class Person {
+    private int age;
+    private String name, gender;
+
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    void display() {
+        System.out.println("Name: " + this.name);
+        System.out.println("Age: " + this.age);
+        System.out.println("Gender: " + this.gender);
+    }
 }
