b2bd6709ac0be18789f6a0644267d88e12138a3c
MdShajeel <94459156+MdShajeel@users.noreply.github.com>
2024-05-30 12:30:08 +0530

    Hospital

--- Hospital/Patient.java
+++ Hospital/Patient.java
@@ -1,19 +1,18 @@
 package Hospital;
-public class Patient extends Person 
-{
-	   private int bedno;
-	   private String des;
 
-	    public Patient(String str,int a,String g,String d, int bno) 
-	    {
-	        super(str,a,g);
-	        this.des= d;
-	        this.bedno=bno;
-	    }
-	    void patdisplay() 
-	    {
-	    	super.perdisplay();
-	        System.out.println("Disease=" + this.des);
-	        System.out.println("Bed No=" + this.bedno);
-	    }
+public class Patient extends Person {
+    private int bedNumber;
+    private String disease;
+
+    public Patient(String name, int age, String gender, String disease, int bedNumber) {
+        super(name, age, gender);
+        this.disease = disease;
+        this.bedNumber = bedNumber;
+    }
+
+    void display() {
+        super.display();
+        System.out.println("Disease: " + this.disease);
+        System.out.println("Bed Number: " + this.bedNumber);
+    }
 }
